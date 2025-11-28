a52b788c46c7e763a1bc122852b10e24701682bf
Snable <mc.dead.f@gmail.com>
2025-11-28 02:38:55 +0000

    Added comments for better documentation, organised files into folders

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,12 @@
+// grab playback icon and audio
 const playbackIcon = document.getElementById("playback-icon");
 const carRevving = document.getElementById("car-revving");
 
+// make cursor show clickable hand
 playbackIcon.style.cursor = "pointer";
 
+// play engine sound when icon is clicked
 playbackIcon.addEventListener("click", () => {
-    carRevving.currentTime = 0;
-    carRevving.play();
+    carRevving.currentTime = 0; // restart audio from the beginning
+    carRevving.play();          // play the sound
 });
